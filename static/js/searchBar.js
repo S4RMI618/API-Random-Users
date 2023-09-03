1bf37a02b9108fdfa598d8e11677f7505207b5c5
S4RMI618 <huessabe@gmail.com>
2023-09-03 18:13:56 -0500

    searchBar finished

--- static/js/searchBar.js
+++ static/js/searchBar.js
@@ -1,18 +1,15 @@
 const d = document;
 export function searchBar(input, selector) {
-    d.addEventListener('keyup', e => {
-        if (e.target.matches(input)) {
-            console.log(e.key)
-            d.querySelectorAll(selector).forEach(el => {
-                el.textContent.toLowerCase().includes(e.target.value)
-                ? el.classList.remove('filter')
-                : el.classList.add('filter')
-            })
-        }
-        
-    })
-    
+  d.addEventListener("keyup", (e) => {
+    if (e.target.matches(input)) {
+      console.log(e.key);
+      d.querySelectorAll(selector).forEach((el) => {
+        el.textContent.toLowerCase().includes(e.target.value)
+          ? el.classList.remove("filter")
+          : el.classList.add("filter");
+      });
+    }
+  });
 }
-
-
+ 
 export default searchBar;
